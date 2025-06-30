27db59217279e2ccc79feae2dbad0af4d69da5ee
daijin <13632657+myonlycode@user.noreply.gitee.com>
2025-06-30 23:21:27 +0800

    移除 .idea 目录的版本控制

---
+++ pos-java/pos-common/src/main/java/org/haut/common/domain/vo/system/AuthorizeVO.java
@@ -0,0 +1,8 @@
+package org.haut.common.domain.vo.system;
+/**
+ *
+ * @author 丁铭瀚
+ * @version 1.0
+ */
+public class AuthorizeVO {
+}
---
+++ pos-java/pos-common/src/main/java/org/haut/common/utils/JwtUtils.java
@@ -0,0 +1,8 @@
+package org.haut.common.utils;
+/**
+ *
+ * @author 丁铭瀚
+ * @version 1.0
+ */
+public class JWTUtils {
+}
