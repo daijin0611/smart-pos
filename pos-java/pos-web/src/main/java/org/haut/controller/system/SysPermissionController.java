633be6cf97dbbbbbfe792be10a11a38508c67b86
丁铭瀚 <13632657+myonlycode@user.noreply.gitee.com>
2025-07-11 01:11:50 +0800

    系统[feat] 添加权限控制器 - 新增SysPermissionController类，处理权限相关请求

---
+++ pos-java/pos-web/src/main/java/org/haut/controller/system/SysPermissionController.java
@@ -0,0 +1,13 @@
+package org.haut.controller.system;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 丁铭瀚
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/system/permission")
+public class SysPermissionController {
+}
