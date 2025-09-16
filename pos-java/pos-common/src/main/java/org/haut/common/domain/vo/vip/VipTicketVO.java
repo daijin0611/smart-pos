f58202e9a7518cf91bb3eddf1d8cc78a1cf72e9b
丁铭瀚 <daijin_0611@qq.com>
2025-09-16 15:39:05 +0800

    refactor( pos-common ): 为 VipTicketVO 类的字段添加 Swagger 注解
    - 在 VipTicketVO 类的各个字段上添加了 @Schema 注解
    - 注解中包含了字段的描述信息，提高了 API 文档的可读性和完整性
    - 为服务项目信息 ServerItemVO 添加了类级别的 @Schema 注解
    - 在 ServerItemVO 的字段上也添加了相应的 @Schema 注解

--- pos-java/pos-common/src/main/java/org/haut/common/domain/vo/vip/VipTicketVO.java
+++ pos-java/pos-common/src/main/java/org/haut/common/domain/vo/vip/VipTicketVO.java
@@ -16,76 +16,94 @@ public class VipTicketVO {
     /**
      * 主键
      */
+    @Schema(description = "主键")
     private Long id;
 
     /**
      * 创建时间
      */
+    @Schema(description = "创建时间")
     private Date createTime;
 
     /**
      * 更新时间
      */
+    @Schema(description = "更新时间")
     private Date updateTime;
 
     /**
      * 删除状态(0 存在，1 删除)
      */
+    @Schema(description = "删除状态(0 存在，1 删除)")
     private Integer isDelete;
 
     /**
      * 备注
      */
+    @Schema(description = "备注")
     private String remark;
 
     /**
      * 优惠券状态（0 启用，1 禁用）
      */
+    @Schema(description = "优惠券状态（0 启用，1 禁用）")
     private Integer ticketStatus;
 
     /**
      * 优惠券名称
      */
+    @Schema(description = "优惠券名称")
     private String ticketName;
 
     /**
      * 优惠券类型
      */
+    @Schema(description = "优惠券类型")
     private Integer ticketType;
 
     /**
      * 有效天数（-1 代表无限期）
      */
+    @Schema(description = "有效天数（-1 代表无限期）")
     private Integer ticketEffectiveTime;
 
     /**
      * 限额满多少元可用
      */
+    @Schema(description = "限额满多少元可用")
     private BigDecimal ticketFullPayment;
 
     /**
      * 优惠券面值
      */
+    @Schema(description = "优惠券面值")
     private BigDecimal ticketValue;
 
     /**
      * 优惠券适用项目
      */
+    @Schema(description = "优惠券适用项目")
     private List<ServerItemVO> serverItems;
 
     /**
      * 门店id
      */
+    @Schema(description = "门店id")
     private Long orgId;
 
     /**
      * 优惠券描述
      */
+    @Schema(description = "优惠券描述")
     private String ticketDescription;
 
     @Data
+    @Schema(description = "服务项目信息")
     public static class ServerItemVO {
+        @Schema(description = "项目ID")
         private Long id;
+        
+        @Schema(description = "项目名称")
         private String itemName;
     }
 }
