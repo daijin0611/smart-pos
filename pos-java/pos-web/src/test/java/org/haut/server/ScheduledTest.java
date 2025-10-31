e640ffb330513c67e6291822917d61fe199b39fc
丁铭瀚 <mhding1024@outlook.com>
2025-10-31 15:05:49 +0800

    test: 更新测试配置和数据库连接池参数
    调整测试类ScheduledTest的注解配置，增加@Ignore注解
    优化application-test.yml中的数据库连接池配置：
    - 增加min-idle连接数
    - 添加test-on-borrow等连接验证配置
    - 新增keep-alive-between-time-millis参数
    添加springdoc和knife4j的API文档配置
    新增幂等性超时配置

--- pos-java/pos-web/src/test/java/org/haut/server/ScheduledTest.java
+++ pos-java/pos-web/src/test/java/org/haut/server/ScheduledTest.java
@@ -1,17 +1,20 @@
 package org.haut.server;
 
+import com.github.xiaoymin.knife4j.annotations.Ignore;
 import jakarta.annotation.Resource;
 import org.haut.server.order.service.OrderSalesSummaryService;
 import org.haut.server.task.ScheduledTaskService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author 丁铭瀚
  * @version 1.0
  */
 @SpringBootTest
+@Ignore
 public class ScheduledTest {
 
     @Autowired
